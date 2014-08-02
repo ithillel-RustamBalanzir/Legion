d08c74fdccbb54af0c2c4a07e217d336883aa692
Alexander Mayboroda <alexander_mayboroda@outlook.com>
2014-08-02 23:09:41 +0300

    First version of ShopServiceImpl class

---
+++ Legion/src/org/hillel/it/legion/service/impl/ShopServiceImpl.java
@@ -0,0 +1,77 @@
+package org.hillel.it.legion.service.impl;
+
+import java.util.List;
+
+import org.hillel.it.legion.model.entity.User;
+import org.hillel.it.legion.model.entity.Product;
+import org.hillel.it.legion.service.ShopService;
+
+public class ShopServiceImpl implements ShopService {
+
+	@Override
+	public void createProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Product> findAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findProductsByCategory(String categoryName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product findProductById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void editProduct(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User findUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void editUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+}
